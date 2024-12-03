2f7b972c86b6996a161dc253cf9bacc0c967f83b
Elbaz <eranelbaz97+github@gmail.com>
2024-12-03 14:45:44 +0200

    basic api implementation for env0 (#4)

---
+++ plugins/backstage-plugin-env0/src/api/client.ts
@@ -0,0 +1,59 @@
+import {createApiRef, ConfigApi} from '@backstage/core-plugin-api';
+import {NotFoundError, NotAllowedError, ServiceUnavailableError} from '@backstage/errors';
+import {Env0Api, Env0ClientApiConfig, Env0ClientApiDependencies, Environment} from "./types";
+
+
+export const env0ApiRef = createApiRef<Env0Api>({
+    id: 'plugin.env0.api',
+});
+
+export class Env0Client implements Env0Api {
+    static fromConfig(
+        _configApi: ConfigApi,
+        dependencies: Env0ClientApiDependencies,
+    ) {
+        const {discoveryApi, fetchApi} = dependencies;
+
+        return new Env0Client({
+            discoveryApi,
+            fetchApi,
+        });
+    }
+
+    constructor(private readonly config: Env0ClientApiConfig) {
+    }
+
+    // https://docs.env0.com/reference/environments-find-all
+    async listEnvironments(projectId:string): Promise<Environment[]> {
+        const url = `${await this.config.discoveryApi.getBaseUrl(
+            'proxy',
+        )}/env0/environments?projectId=${projectId}&isActive=true`;
+        const project = await this.request(url, {
+            method: 'GET',
+        })
+        return project.json();
+    }
+
+    private async request(
+        url: string,
+        options: RequestInit,
+    ): Promise<Response> {
+        const response = await this.config.fetchApi.fetch(url, options);
+
+        if (response.status === 401) {
+            throw new NotAllowedError(await response.text());
+        }
+
+        else if (response.status === 404) {
+            throw new NotFoundError(await response.text());
+        }
+        else if (response.status === 504) {
+            throw new ServiceUnavailableError(await response.text());
+        }
+
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+        return response;
+    }
+}
---
+++ plugins/backstage-plugin-env0/src/api/types.d.ts
@@ -0,0 +1,20 @@
+import {DiscoveryApi, FetchApi} from "@backstage/core-plugin-api";
+
+export type Env0ClientApiDependencies = {
+    discoveryApi: DiscoveryApi;
+    fetchApi: FetchApi;
+};
+
+export type Env0ClientApiConfig = Env0ClientApiDependencies & {};
+
+export interface Environment {
+    name: string;
+    organizationId: string;
+    projectId: string;
+    userId: string;
+    workspaceName: string;
+}
+
+export type Env0Api = {
+    listEnvironments(projectId: string): Promise<Environment[]>;
+}
