d7f0bcc3a23c17d1f324307ca6ead31ab856dd7a
elbaz <eranelbaz97+github@gmail.com>
2024-12-03 17:31:00 +0200

    rollback
    Signed-off-by: elbaz <eranelbaz97+github@gmail.com>

--- plugins/backstage-plugin-env0/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/backstage-plugin-env0/src/components/ExampleComponent/ExampleComponent.tsx
@@ -10,10 +10,8 @@ import {
     SupportButton,
 } from '@backstage/core-components';
 import {ExampleFetchComponent} from '../ExampleFetchComponent';
-import {Env0EnvironmentDetailsCard} from "../env0-environment-details-card";
 
 export const ExampleComponent = () => {
-    return <Env0EnvironmentDetailsCard/>
     return (
 
         <Page themeId="tool">
