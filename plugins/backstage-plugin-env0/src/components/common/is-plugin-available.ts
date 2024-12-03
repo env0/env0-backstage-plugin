df4566c81e8b33841e49928a6692b279640d5e98
Elbaz <eranelbaz97+github@gmail.com>
2024-12-03 12:29:18 +0200

    add `isEnv0Available` method (#5)

---
+++ plugins/backstage-plugin-env0/src/components/common/is-plugin-available.ts
@@ -0,0 +1,11 @@
+import { Entity } from '@backstage/catalog-model';
+
+export const ENV0_ORGANIZATION_ANNOTATION = 'env0.com/organization-id';
+export const ENV0_ENVIRONMENT_ANNOTATION = 'env0.com/environment-id';
+
+export const isEnv0Available = (entity: Entity) =>
+    Boolean(
+        entity.metadata.annotations?.[ENV0_ORGANIZATION_ANNOTATION] ||
+        entity.metadata.annotations?.[ENV0_ENVIRONMENT_ANNOTATION]
+    );
+
