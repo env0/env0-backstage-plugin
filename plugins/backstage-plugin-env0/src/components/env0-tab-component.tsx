d1e60ccfc0e7210be4247c2c148c26b9529a5d45
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2024-12-04 12:44:52 +0200

    Chore: Add env0 tab component and add it to backstage entity page (#6)
    * Add env0 tab component and add it to backstage entity page
    
    * Add usage in isEnv0Available
    
    * change to prettierrc.js

---
+++ plugins/backstage-plugin-env0/src/components/env0-tab-component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Content, Page } from '@backstage/core-components';
+
+export const Env0TabComponent = () => (
+  <Page themeId="tool">
+    <Content>
+      <h1>Env0 Tab</h1>
+      <p>
+        This is a plugin that integrates with Env0 to manage your environments
+        and deployments.
+      </p>
+    </Content>
+  </Page>
+);
