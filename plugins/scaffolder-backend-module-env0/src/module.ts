cee843036d4293fb6c9a95a15d044ca3ec6db3d5
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-04 14:15:23 +0200

    remove comment

--- plugins/scaffolder-backend-module-env0/src/module.ts
+++ plugins/scaffolder-backend-module-env0/src/module.ts
@@ -1,10 +1,7 @@
 import { createBackendModule } from "@backstage/backend-plugin-api";
 import { scaffolderActionsExtensionPoint  } from '@backstage/plugin-scaffolder-node/alpha';
-import { createExampleAction } from "./actions/example";
+import { createExampleAction } from "./actions/deployAction";
 
-/**
- * A backend module that registers the action into the scaffolder
- */
 export const scaffolderModule = createBackendModule({
   moduleId: 'example-action',
   pluginId: 'scaffolder',
