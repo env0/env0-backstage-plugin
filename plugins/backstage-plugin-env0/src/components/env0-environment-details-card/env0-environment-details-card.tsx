16921b6ab3520ffde8dd9fea44c97c0d9a49276e
elbaz <eranelbaz97+github@gmail.com>
2024-12-05 17:34:32 +0200

    useAsyncRetry instead of useAsync
    Signed-off-by: elbaz <eranelbaz97+github@gmail.com>

--- plugins/backstage-plugin-env0/src/components/env0-environment-details-card/env0-environment-details-card.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-environment-details-card/env0-environment-details-card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useApi } from '@backstage/core-plugin-api';
 import { Env0Api } from '../../api/types';
-import useAsync from 'react-use/lib/useAsync';
+import useAsync from 'react-use/lib/useAsyncRetry';
 import { ErrorContainer } from '../common/error-container';
 import {
   InfoCard,
@@ -14,31 +14,38 @@ import isEmpty from 'lodash/isEmpty';
 import { getGitProvider, getShortenRepo } from './get-shorten-repo';
 import { VcsIcon } from './vcs-icon';
 import { env0ApiRef } from '../../api';
-import { CardHeader, styled } from '@material-ui/core';
+import { CardHeader, styled, Button } from '@material-ui/core';
+import Cached from '@material-ui/icons/Cached';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { ENV0_ENVIRONMENT_ANNOTATION } from '../common/is-plugin-available';
 import { Env0Icon } from '../env0-icon';
 
 type CardProps = {
   children: React.ReactNode;
+  onAction?: () => void;
 };
 
 const VcsLinkContainer = styled('div')(() => ({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
-}))
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+}));
 
 const StyledLink = styled(Link)(() => ({
-    paddingLeft: '5px'
-}))
+  paddingLeft: '5px',
+}));
 
-const Env0Card = ({ children, ...rest }: CardProps) => (
+const Env0Card = ({ children, onAction, ...rest }: CardProps) => (
   <InfoCard {...rest}>
     <CardHeader
       title="env0"
       avatar={<Env0Icon style={{ fontSize: 40 }} />}
       titleTypographyProps={{ variant: 'h5' }}
+      action={
+        <Button onClick={onAction}>
+          <Cached />
+        </Button>
+      }
     />
     {children}
   </InfoCard>
@@ -92,7 +99,7 @@ export const Env0EnvironmentDetailsCard = () => {
   );
 
   return (
-    <Env0Card>
+    <Env0Card onAction={() => retry()}>
       <StructuredMetadataTable
         dense
         metadata={{
