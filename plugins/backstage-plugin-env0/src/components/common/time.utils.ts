a93e2e08751dca08ce79571084caa6facaff3278
ItamarMalka <itamar.malka@env0.com>
2024-12-05 18:57:02 +0200

    make long names and drop redundant logic

--- plugins/backstage-plugin-env0/src/components/common/time.utils.ts
+++ plugins/backstage-plugin-env0/src/components/common/time.utils.ts
@@ -8,22 +8,12 @@ export const formatDatetime = (value: string | Date | undefined) =>
     minute: 'numeric',
   });
 
-export const parseTimerElapsedTime = (seconds: number) => {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
+export const parseTimerElapsedTime = (elapsedTimeInSeconds: number) => {
+  const elapsedHours = Math.floor(elapsedTimeInSeconds / 3600);
+  const elapsedMinutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+  const elapsedSeconds = Math.floor(elapsedTimeInSeconds % 60);
 
-  const config = {
-    h: true,
-    m: true,
-    s: true,
-  };
-
-  return [config.h && h, config.m && m, config.s && s]
-    .filter(
-      (maybeNumber: false | number): maybeNumber is number =>
-        maybeNumber !== false,
-    )
+  return [elapsedHours, elapsedMinutes, elapsedSeconds]
     .map(timeUnit => (timeUnit < 10 ? `0${timeUnit}` : String(timeUnit)))
     .join(':');
 };
