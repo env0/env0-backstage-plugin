6e72a6876326c16fbe3a07f80a1670a66e3c4fa9
ItamarMalka <itamar.malka@env0.com>
2024-12-05 18:22:33 +0200

    use the entity id

--- plugins/backstage-plugin-env0/src/components/env0-tab-component.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-tab-component.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import { Content, Page } from '@backstage/core-components';
 import { Env0DeploymentTable } from './env0-deployment-table';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEntity } from '@backstage/plugin-catalog-react';
+import { ENV0_ENVIRONMENT_ANNOTATION } from './common/is-plugin-available';
 
 const queryClient = new QueryClient();
 
-export const Env0TabComponent = () => (
-  <Page themeId="tool">
-    <Content>
-      <QueryClientProvider client={queryClient}>
-        <Env0DeploymentTable
-          environmentId={'94da6324-61b1-46ac-95fb-4c00e4e18c85'}
-        />
-      </QueryClientProvider>
-    </Content>
-  </Page>
-);
+export const Env0TabComponent = () => {
+  const { entity } = useEntity();
+  const environmentId =
+    entity.metadata.annotations?.[ENV0_ENVIRONMENT_ANNOTATION];
+
+  if (!environmentId) {
+    return (
+      <Page themeId="tool">
+        <Content>
+          <div>Environment ID not found</div>
+        </Content>
+      </Page>
+    );
+  }
+
+  return (
+    <Page themeId="tool">
+      <Content>
+        <QueryClientProvider client={queryClient}>
+          <Env0DeploymentTable environmentId={environmentId} />
+        </QueryClientProvider>
+      </Content>
+    </Page>
+  );
+};
