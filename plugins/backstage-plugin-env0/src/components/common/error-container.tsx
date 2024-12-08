b0cb24a77a8d673c290eb6ae2363eea654c0a284
ItamarMalka <itamar.malka@env0.com>
2024-12-08 14:52:20 +0200

    fix erroring not specific import

--- plugins/backstage-plugin-env0/src/components/common/error-container.tsx
+++ plugins/backstage-plugin-env0/src/components/common/error-container.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import {NotFoundError, NotAllowedError, ServiceUnavailableError} from '@backstage/errors';
-import {Alert} from '@material-ui/lab';
+import {
+  NotFoundError,
+  NotAllowedError,
+  ServiceUnavailableError,
+} from '@backstage/errors';
+import Alert from '@material-ui/lab/Alert';
 
-export const ErrorContainer = ({error}: { error: Error }) => {
-    if (error.constructor === NotAllowedError) {
-        return <Alert severity="error">
-            You are not authorized to view this information.
-        </Alert>
-    } else if (error.constructor === NotFoundError) {
-        return <Alert severity="info">
-            No information found for this entity.
-        </Alert>
-    } else if (error.constructor === NotAllowedError) {
-        return <Alert severity="info">
-            You are unauthorized to view this information.
-        </Alert>
-    }  else if (error.constructor === ServiceUnavailableError) {
-        return <Alert severity="info">
-            env0 is currently unavailable
-        </Alert>
-    } else {
-        return <Alert severity="error">
-            Error encountered while fetching information. {error.message}
-        </Alert>
-    }
-}
+export const ErrorContainer = ({ error }: { error: Error }) => {
+  if (error.constructor === NotAllowedError) {
+    return (
+      <Alert severity="error">
+        You are not authorized to view this information.
+      </Alert>
+    );
+  } else if (error.constructor === NotFoundError) {
+    return <Alert severity="info">No information found for this entity.</Alert>;
+  } else if (error.constructor === NotAllowedError) {
+    return (
+      <Alert severity="info">
+        You are unauthorized to view this information.
+      </Alert>
+    );
+  } else if (error.constructor === ServiceUnavailableError) {
+    return <Alert severity="info">env0 is currently unavailable</Alert>;
+  }
+  return (
+    <Alert severity="error">
+      Error encountered while fetching information. {error.message}
+    </Alert>
+  );
+};
