599dbc39b8e392395dc5159fe21003cb319f81dc
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2024-12-08 16:20:25 +0200

    Chore: Use error container for errors (#17)
    * Use error container for errors

--- plugins/backstage-plugin-env0/src/components/env0-tab-component.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-tab-component.tsx
@@ -3,6 +3,7 @@ import { Content, Page } from '@backstage/core-components';
 import { Env0DeploymentTable } from './env0-deployment-table';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { ENV0_ENVIRONMENT_ANNOTATION } from './common/is-plugin-available';
+import { ErrorContainer } from './common/error-container';
 
 export const Env0TabComponent = () => {
   const { entity } = useEntity();
@@ -13,7 +14,7 @@ export const Env0TabComponent = () => {
     return (
       <Page themeId="tool">
         <Content>
-          <div>Environment ID not found</div>
+          <ErrorContainer error={new Error('No environment ID found')} />
         </Content>
       </Page>
     );
