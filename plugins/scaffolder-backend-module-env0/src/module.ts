dd164669fd462aeecc8385ad5ec7ae1a544fd7db
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-09 13:31:35 +0200

    add create env action to scaffolder backend

--- plugins/scaffolder-backend-module-env0/src/module.ts
+++ plugins/scaffolder-backend-module-env0/src/module.ts
@@ -1,6 +1,6 @@
-import { createBackendModule } from "@backstage/backend-plugin-api";
-import { scaffolderActionsExtensionPoint  } from '@backstage/plugin-scaffolder-node/alpha';
-import { createExampleAction } from "./actions/create-environment";
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+import { createEnv0EnvironmentAction } from './actions/create-environment';
 
 export const scaffolderModule = createBackendModule({
   moduleId: 'example-action',
@@ -8,11 +8,11 @@ export const scaffolderModule = createBackendModule({
   register({ registerInit }) {
     registerInit({
       deps: {
-        scaffolderActions: scaffolderActionsExtensionPoint
+        scaffolderActions: scaffolderActionsExtensionPoint,
       },
-      async init({ scaffolderActions}) {
-        scaffolderActions.addActions(createExampleAction());
-      }
+      async init({ scaffolderActions }) {
+        scaffolderActions.addActions(createEnv0EnvironmentAction());
+      },
     });
   },
-})
+});
