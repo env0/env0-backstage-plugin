fc235f1df67987bc460a99ab00e4b4236ce6f1e3
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-09 16:00:09 +0200

    export shared vars schema

---
+++ plugins/scaffolder-backend-module-env0/src/actions/common/schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const variablesSchema = z.object({
+  id: z
+    .string({
+      description:
+        'The ID of an existing variable to override. When not used, will create a new variable',
+    })
+    .optional(),
+  name: z.string({ description: 'The name of the variable' }),
+  value: z.string({ description: 'The value of the variable' }).optional(),
+  type: z.union([z.literal(1), z.literal(0)], {
+    description: 'The type of the variable; 0 - TF Var, 1 - Env Var',
+  }),
+});
