61394cb64565e7e3665ec7944ebc947f8a700a64
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-09 10:25:46 +0200

    initial create env action

--- plugins/scaffolder-backend-module-env0/src/actions/create-environment.ts
+++ plugins/scaffolder-backend-module-env0/src/actions/create-environment.ts
@@ -1,34 +1,42 @@
 import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import { z } from 'zod';
 
-export function createExampleAction() {
-  // For more information on how to define custom actions, see
-  //   https://backstage.io/docs/features/software-templates/writing-custom-actions
-  return createTemplateAction<{
-    myParameter: string;
-  }>({
-    id: 'acme:example',
-    description: 'Runs an example action',
+type Args = z.infer<typeof schema>;
+
+const schema = z.object({
+  name: z.string({ description: 'The name of the environment' }),
+  templateId: z.string({
+    description: 'The ID of the template to be deployed',
+  }),
+  projectId: z.string({
+    description: 'The ID of the project to place the new environment on',
+  }),
+  variables: z
+    .object({
+      id: z
+        .string({
+          description:
+            'The ID of an existing variable to override. When not used, will create a new variable',
+        })
+        .optional(),
+      name: z.string({ description: 'The name of the variable' }),
+      value: z.string({ description: 'The value of the variable' }).optional(),
+      type: z.union([z.literal(1), z.literal(0)], {
+        description: 'The type of the variable; 0 - TF Var, 1 - Env Var',
+      }),
+    })
+    .optional(),
+});
+
+export function createEnv0EnvironmentAction() {
+  return createTemplateAction<Args>({
+    id: 'env0:create:environment',
+    description: 'Creates a new env0 environment',
     schema: {
-      input: {
-        type: 'object',
-        required: ['myParameter'],
-        properties: {
-          myParameter: {
-            title: 'An example parameter',
-            description: "This is an example parameter, don't set it to foo",
-            type: 'string',
-          },
-        },
-      },
+      input: schema,
     },
     async handler(ctx) {
-      ctx.logger.info(
-        `Running example template with parameters: ${ctx.input.myParameter}`,
-      );
-
-      if (ctx.input.myParameter === 'foo') {
-        throw new Error(`myParameter cannot be 'foo'`);
-      }
+      ctx.logger.info(`Creating env0 environment`);
 
       await new Promise(resolve => setTimeout(resolve, 1000));
     },
