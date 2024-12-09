b66ecdb4fc9990775c4209dd55957f6104c6a994
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-09 16:00:43 +0200

    add new redploy action

---
+++ plugins/scaffolder-backend-module-env0/src/actions/redeploy-environment.ts
@@ -0,0 +1,30 @@
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import { z } from 'zod';
+import { apiClient } from './common/api-client';
+import { variablesSchema } from './common/schema';
+
+type Args = z.infer<typeof schema>;
+
+const schema = z.object({
+  id: z.string({ description: 'The ID of the environment to redeploy' }),
+  variables: variablesSchema.optional(),
+});
+
+export function createEnv0EnvironmentAction() {
+  return createTemplateAction<Args>({
+    id: 'env0:environment:redeploy',
+    description: 'Redeploys an existing env0 environment',
+    schema: {
+      input: schema,
+    },
+    async handler(ctx) {
+      ctx.logger.info(`Redeploying env0 environment`);
+
+      await apiClient.deployEnvironment(ctx.input.id, {
+        variables: ctx.input.variables,
+      });
+
+      ctx.logger.info(`env0 environment re-deploy initiated successfully`);
+    },
+  });
+}
