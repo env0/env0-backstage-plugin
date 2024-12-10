ebba17cda2f15b3da1d1112f856e777957c2ebc4
Wassap124 <alonkazimirsky@gmail.com>
2024-12-10 22:21:14 +0200

    further finalizing status component before actually putting styles

--- plugins/backstage-plugin-env0/src/components/env0-status/Status.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-status/Status.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { makeStyles, Theme, Chip, Paper } from '@material-ui/core';
+import { makeStyles, Chip, ChipProps } from '@material-ui/core';
 
-interface StatusProps {
+interface StatusProps extends ChipProps {
   status: string;
 }
 
+/**
+  background-color: ${({ status }) => statusToTextAndColor[status].bgColor};
+  color: ${({ status }) => statusToTextAndColor[status].color};
+ */
+
 const useStyles = makeStyles({
-  statusContainer: props => ({
+  statusContainer: (props: StatusProps) => ({
     borderRadius: '4px',
     fontWeight: 600,
     fontSize: '12px',
@@ -14,17 +19,18 @@ const useStyles = makeStyles({
     padding: '0 10.5px',
     textAlign: 'center',
     maxWidth: '140px',
+    backgroundColor: props.status === 'ACTIVE' ? '#00C853' : '#FF8A65',
+    color: props.status === 'ACTIVE' ? '#00C853' : '#FF8A65',
+    minWidth: '120px',
   }),
 });
 
-const Status: React.FC<StatusProps> = ({ status }) => {
-  return <Chip label={status} />;
+const Status: React.FC<StatusProps> = ({ status, ...props }) => {
+  const className = useStyles({ status });
+
+  return (
+    <Chip className={className.statusContainer} label={status} {...props} />
+  );
 };
 
 export default Status;
-
-/**
-  background-color: ${({ status }) => statusToTextAndColor[status].bgColor};
-  color: ${({ status }) => statusToTextAndColor[status].color};
-  min-width: ${({ minWidth }) => minWidth || '120px'};
- */
