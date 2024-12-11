e311b0f8aaa6c799fe8e55b5f344812222036520
Wassap124 <alonkazimirsky@gmail.com>
2024-12-11 15:46:53 +0200

    use styled instead of makeStyles

--- plugins/backstage-plugin-env0/src/components/env0-status/Status.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-status/Status.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { makeStyles, Chip, ChipProps } from '@material-ui/core';
+import { Chip, ChipProps, styled } from '@material-ui/core';
 import { statusToTextAndColor } from './status-map';
 
 interface StatusProps extends ChipProps {
   status: keyof typeof statusToTextAndColor;
 }
 
-const useStyles = makeStyles({
-  statusContainer: (props: StatusProps) => ({
+const StyledChip = styled(Chip)(
+  ({ status }: { status: keyof typeof statusToTextAndColor }) => ({
     borderRadius: '4px',
     fontWeight: 600,
     fontSize: '12px',
@@ -15,17 +15,19 @@ const useStyles = makeStyles({
     padding: '0 10.5px',
     textAlign: 'center',
     maxWidth: '140px',
-    backgroundColor: statusToTextAndColor[props.status].bgColor,
-    color: statusToTextAndColor[props.status].color,
+    backgroundColor: statusToTextAndColor[status].bgColor,
+    color: statusToTextAndColor[status].color,
     minWidth: '120px',
   }),
-});
+);
 
 const Status: React.FC<StatusProps> = ({ status, ...props }) => {
-  const className = useStyles({ status });
-
   return (
-    <Chip className={className.statusContainer} label={statusToTextAndColor[status].text} {...props} />
+    <StyledChip
+      status={status}
+      label={statusToTextAndColor[status].text}
+      {...props}
+    />
   );
 };
 
