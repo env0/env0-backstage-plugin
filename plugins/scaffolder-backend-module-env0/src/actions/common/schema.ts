8656025d8edcd1b2f2a0f0a895c7f933ee820884
ItamarMalka <itamar.malka@env0.com>
2024-12-13 00:32:02 +0200

    drop not looked at values and make the schema passthrough extra props

--- plugins/scaffolder-backend-module-env0/src/actions/common/schema.ts
+++ plugins/scaffolder-backend-module-env0/src/actions/common/schema.ts
@@ -2,22 +2,14 @@ import { z } from 'zod';
 
 export const variablesSchema = z
   .object({
-    id: z
-      .string({
-        description:
-          'The ID of an existing variable to override. When not used, will create a new variable',
-      })
-      .optional(),
+    id: z.string({
+      description:
+        'The ID of an existing variable to override. When not used, will create a new variable',
+    }),
     name: z.string({ description: 'The name of the variable' }),
     value: z.string({ description: 'The value of the variable' }).optional(),
-    type: z.union([z.literal(1), z.literal(0)], {
-      description: 'The type of the variable; 0 - TF Var, 1 - Env Var',
-    }),
-    isSensitive: z.boolean({
-      description: 'Whether the variable is sensitive',
-    }),
-    isRequired: z.boolean({ description: 'Whether the variable is required' }),
   })
+  .catchall(z.any())
   .array();
 
 export const commentSchema = z.string({
