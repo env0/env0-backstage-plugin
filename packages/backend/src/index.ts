f3b51d1c7866943e66728cff5ecf48458d64e38d
Yaron Yarimi <yaron.yarimi@env0.com>
2024-12-15 16:02:54 +0200

    add gh plugin

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -51,5 +51,9 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+// env0
 backend.add(import('@env0/backstage-scaffolder-backend-env0'));
+
+// GitHub
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.start();
