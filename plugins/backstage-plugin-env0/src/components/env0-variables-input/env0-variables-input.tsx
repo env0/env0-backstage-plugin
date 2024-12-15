64ee23ea0e44e1d00ec9fac2f5122e996a1a8b22
elbaz <eranelbaz97+github@gmail.com>
2024-12-15 17:59:43 +0200

    add warning icon
    Signed-off-by: elbaz <eranelbaz97+github@gmail.com>

--- plugins/backstage-plugin-env0/src/components/env0-variables-input/env0-variables-input.tsx
+++ plugins/backstage-plugin-env0/src/components/env0-variables-input/env0-variables-input.tsx
@@ -16,6 +16,7 @@ import InfoRounded from '@material-ui/icons/InfoRounded';
 
 import { z } from 'zod';
 import type { Variable } from '../../api/types';
+import Warning from '@material-ui/icons/Warning';
 
 const VariableContainer = styled('div')(() => ({
   display: 'grid',
@@ -184,10 +185,18 @@ export const Env0VariablesInput = ({
               <Typography
                 variant="body1"
                 style={{
+                  display: 'flex',
                   minWidth: '150px',
                   fontWeight: 'bold',
                 }}
               >
+                {variable.isSensitive && (
+                  <Tooltip
+                    title="Senstive variable"
+                  >
+                    <Warning />
+                  </Tooltip>
+                )}
                 {variable.name}:
               </Typography>
               <InputAndInfoIconContainer>
